eb7b547a4eeb9fb061d6430fff8092fd87e2eb3f
Bruno Peres <brunomacielp93@gmail.com>
2014-10-19 15:16:10 -0200

    Remover /absorcao da url

--- custeio_absorcao/custeio_absorcao/urls.py
+++ custeio_absorcao/custeio_absorcao/urls.py
@@ -15,5 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^absorcao/', include('absorcao.urls')),
+    url(r'', include('absorcao.urls')),
 )
