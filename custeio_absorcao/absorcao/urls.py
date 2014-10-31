4a901397c992caf1c36880ceb8e7432e2d0ab6bd
Bruno Vinicius Avila Machado <bruno@localhost.localdomain>
2014-10-31 20:32:42 -0200

    Colocando paginas e views dos custos diretos

--- custeio_absorcao/absorcao/urls.py
+++ custeio_absorcao/absorcao/urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns('',
                        url(r'^produto/$', views.produto_index, name='produto'),
                        url(r'^produto/edit/(?P<id_produto>\w+)/$', views.produto_edit, name='produto_edit'),
                        url(r'^produto/(?P<id_produto>\w+)/$', views.produto_view, name='produto_view'),
+                       url(r'^custo_direto/$', views.custo_direto_index, name='custo_direto'),
+                       url(r'^custo_direto/edit/(?P<mes>\w+)/$', views.custo_direto_edit, name='custo_direto_edit'),
                        )
